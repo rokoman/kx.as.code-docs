4b4a4ed63df93bcab301b3d6a19e27593e246f39
Patrick Delamere <patrick@delamere.de>
2020-12-21 15:59:50 +0100

    Initial commit of KX.AS.CODE Docs

---
+++ createKxDocs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+
+. /etc/environment
+export VM_USER=$vmUser
+
+sourceRepoLocation=/home/$VM_USER/Documents/kx.as.code_source
+tempLocation=/home/$VM_USER/Documents/kx.as.code_docs/tmp
+targetMkDocsLocation=/home/$VM_USER/Documents/kx.as.code_docs/docs
+
+mkdir -p $tempLocation
+mkdir -p $targetMkDocsLocation
+cp -rf $sourceRepoLocation/* $tempLocation/
+cp -rf /home/$VM_USER/Documents/kx.as.code_docs/images $targetMkDocsLocation
+cp -rf /home/$VM_USER/Documents/kx.as.code_source/images/* $targetMkDocsLocation/images/
+find $tempLocation -type f -iname "*.md" -exec sed -i '/Zero2Hero_Logo_Black\.png/d' {} +
+find $tempLocation -type f -iname "*.md" -exec sed -i '/kxascode_logo_black_small\.png/d' {} +
+find $tempLocation -type f -iname "*.md" -exec sed -i '/# README/d' {} +
+
+files=$(find $tempLocation -mindepth 1 -iregex '.*\.\(md\|png\|jpg\)$' -not -path "$tempLocation/base-vm/*" -printf '%P\n')
+
+for file in $files
+do
+  if [[ "$file" != *'TEMPLATE'* ]] && [[ "$file" != *'WIP'* ]]; then
+    echo "File: $file"
+    PATH_DEPTH=$(echo $file | grep -o "/" | wc -l)
+    if [[ $PATH_DEPTH < 1 ]]; then
+      echo "Depth only $PATH_DEPTH. Copy file only without creating a directory"
+      cp -f $tempLocation/$file $targetMkDocsLocation
+    else
+      echo "Path Depth => 1. Creating file and directory"
+      newFile=$(printf '%s' "$file" | sed 's/[_]//g' | sed -e 's/\b\(.\)/\u\1/g')
+      newFilePath="${newFile%/*}/"
+      newFilePath=$(echo $newFilePath | sed 's/\/*$//g')
+      if [[ "$newFilePath" =~ '/' ]]; then
+        echo "Creating directory $newFilePath"
+        mkdir -p $(dirname $targetMkDocsLocation/$newFilePath)
+      fi
+      if [[ "$file" =~ 'README.md' ]] && [[ $PATH_DEPTH > 2 ]]; then
+        cp -f $tempLocation/$file $targetMkDocsLocation/$newFilePath".md"
+      else
+        cp -f $tempLocation/$file $(dirname $targetMkDocsLocation/$newFilePath)
+      fi
+    fi
+  fi
+done
+
+rm -rf $tempLocation
